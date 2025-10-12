09ed19b4562ef51695b3d8fff45407dd833b3cac
haroldbaes17 <hbarrantese@ucenfotec.ac.cr>
2025-10-12 17:09:16 -0600

    Estructura del proyecto creada

---
+++ src/test/java/io/github/haroldbaes17/minecraftfacts/MinecraftFactsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.haroldbaes17.minecraftfacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftFactsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
